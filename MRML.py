91a9133746352fc74ced6ebb66f86d4a31d26918
santi <santiago.penate.vera@gmail.com>
2017-07-03 20:58:53 +0200

    Inconsistency of the data

--- MRML.py
+++ MRML.py
@@ -32,6 +32,8 @@ def df_analysis(df: pd.DataFrame):
                 np.float16, np.float32, np.float64,
                 np.double, np.complex)
 
+    dictry = dict()
+
     for cname in df.columns.values:
 
         row = [None] * 8
@@ -77,7 +79,9 @@ def df_analysis(df: pd.DataFrame):
             row[6] = 0
             warn(cname + ' is an object! This is indicative of mixed data types!!')
 
-            is_num, p = is_the_column_numeric(df, cname, threshold=0.6)
+            is_num, p, bad_idx = is_the_column_numeric(df, cname, threshold=0.6)
+
+            dictry[cname] = bad_idx
 
             if is_num:
                 row[7] = p
@@ -94,7 +98,7 @@ def df_analysis(df: pd.DataFrame):
         dta.append(row)
 
     cls = ['Type', 'General', 'Max', 'Min', 'Mean', 'Range', 'NaN %', 'Numerical likelihood']
-    return pd.DataFrame(data=dta, columns=cls, index=df.columns.values)
+    return pd.DataFrame(data=dta, columns=cls, index=df.columns.values), dictry
 
 
 def is_the_column_numeric(df, col_name, threshold=0.7):
@@ -102,6 +106,7 @@ def is_the_column_numeric(df, col_name, threshold=0.7):
     values = df[col_name].values
     n = len(values)
     num_counter = 0
+    indx = list()
 
     for i, v in enumerate(values):
 
@@ -113,14 +118,15 @@ def is_the_column_numeric(df, col_name, threshold=0.7):
                 complex(v)
                 num_counter += 1
             except:
-                pass
+                # if I reach this point, it means this is not a number
+                indx.append(i)
 
     p = num_counter / n
 
     if p > threshold:
-        return True, p
+        return True, p, indx
     else:
-        return False, p
+        return False, p, indx
 
 
 def correct_missing_values(df: pd.DataFrame, c_name, option: MissingValueOptions):
@@ -140,34 +146,58 @@ def correct_missing_values(df: pd.DataFrame, c_name, option: MissingValueOptions
         df.fillna(method='pad', inplace=True)
 
 
+def make_em_nans(df: pd.DataFrame, lst_idx, col_name):
+
+    # v = df[col_name].values
+    # v[lst_idx] = np.nan
+    df[col_name].values[lst_idx] = np.nan
+
+
 def clean(df, missing_value_option: MissingValueOptions=MissingValueOptions.Keep):
 
-    analysis_result_df = df_analysis(df)
+    analysis_result_df, bad_idx_per_column = df_analysis(df)
 
     print(analysis_result_df)
 
     nan_perc = analysis_result_df['NaN %'].values
     num_likelihood = analysis_result_df['Numerical likelihood'].values
 
-    for r in range(len(analysis_result_df)):
+    for c in range(len(analysis_result_df)):
 
-        cname = df.columns.values[r]
+        cname = df.columns.values[c]
 
         # if analysis_result_df['Type'].values[r] == object:
-        if nan_perc[r] == 0 and num_likelihood[r] == 1:
+        if nan_perc[c] == 0 and num_likelihood[c] == 1:
             pass  # It means there are no missing values and it is correct that it is a number column
-        elif nan_perc[r] == 0 and (0 < num_likelihood[r] < 1):
+
+        elif nan_perc[c] == 0 and (0 < num_likelihood[c] < 1):
             print('Inconsistent data! Do SOMETHING!', cname)
             # TODO: implement method
-        elif nan_perc[r] > 0 and num_likelihood[r] == 1:
+            bad_idx = bad_idx_per_column[cname]
+
+            if num_likelihood[c] > 0.7:
+                print('\t Inconsistent number column!')
+                # even though up to 30% of the data in this column is garbage, the majority of the data in this
+                # column is numeric, so we will throw away the rest of the values
+                if missing_value_option is not MissingValueOptions.Keep:
+                    make_em_nans(df=df, lst_idx=bad_idx, col_name=cname)
+                    correct_missing_values(df, c_name=cname, option=missing_value_option)
+                else:
+                    pass
+            else:
+                print('\t Inconsistent string column!')  # TODO: implement method
+
+        elif nan_perc[c] > 0 and num_likelihood[c] == 1:
             print('There are missing values in this column:', cname)
             correct_missing_values(df, c_name=cname, option=missing_value_option)
 
-        elif nan_perc[r] > 0 and (0 < num_likelihood[r] < 1):
+        elif nan_perc[c] > 0 and (0 < num_likelihood[c] < 1):
             print('There are missing values AND inconsistent data!', cname)
             # TODO: implement methods
-        elif nan_perc[r] == 0 and num_likelihood[r] == 0:
+
+        elif nan_perc[c] == 0 and num_likelihood[c] == 0:
             print('This is likely a string column', cname)
+
         else:
             print('There is something really wrong!', cname)
 
@@ -183,3 +213,6 @@ if __name__ == '__main__':
     clean(data_, MissingValueOptions.Replace)
 
     print(data_.head(20))
+
+    df_ref, _ = df_analysis(data_)
+    print(df_ref)
